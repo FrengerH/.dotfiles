c640a4407b34679bb80bda8ec9c30ff13e95f26d
FrengerH <frengerh@hfmail.nl>
2022-03-30 23:40:43 +0200

    dwm

---
+++ dwm/.config/dwm/autostart.sh
@@ -0,0 +1,5 @@
+#picom --config $HOME/.config/compton.conf > /dev/null &
+autorandr -c
+nitrogen --set-zoom-fill --head=0 $HOME/.config/leftwm/themes/leftwm-theme-dracula-rounded/background.jpg
+#sleep 1 
+dwmblocks
