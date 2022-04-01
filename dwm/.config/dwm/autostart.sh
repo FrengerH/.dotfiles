4203b8da1411f945d3ecfaf495b02bf77a8af202
FrengerH <frengerh@hfmail.nl>
2022-04-01 11:49:47 +0200

    config updates

--- dwm/.config/dwm/autostart.sh
+++ dwm/.config/dwm/autostart.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
 #picom --config $HOME/.config/compton.conf > /dev/null &
 autorandr -c
-nitrogen --set-zoom-fill --head=0 $HOME/.config/leftwm/themes/leftwm-theme-dracula-rounded/background.jpg
-#sleep 1 
+
+monitors=$(xrandr --listmonitors | wc -l)
+
+for i in $(seq $((monitors-1)))
+do
+    nitrogen --set-zoom-fill --head=$((i-1)) $HOME/.config/leftwm/themes/leftwm-theme-dracula-rounded/background.jpg
+done
+
+sleep 1 
 dwmblocks
